f4d81743909ba4cf2d687eeafc2aadf410be92ea
almostgone47 <almostgone47@hotmail.com>
2020-02-08 15:28:25 -0800

    last changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 3003;
 const morgan = require('morgan');
 const path = require('path');
 const db = require('../db.js');
@@ -12,7 +12,7 @@ app.get('/reviews', (req, res) => {
     if (err) {
       res.status(400).send();
     } else {
-      res.send(data);
+      res.send(data)
     }
   });
 });
